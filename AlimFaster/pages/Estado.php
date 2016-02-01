612a2e4c67c397c06402851c03b6c4bae6b7935c
Edwin Estacio <p210estacioe@gmail.com>
2016-01-31 22:47:35 -0500

    declaracion de variables estado

---
+++ AlimFaster/pages/Estado.php
@@ -0,0 +1,16 @@
+<?php 
+class Estado
+{
+	private $codigo_estado
+	private $descripcion
+	private $codigo_restaurante
+	private $codigo_cliente
+	private $codigo_usuario
+	private $codigo_producto
+	private $codigo_tarjeta
+	private $numero_mesa
+
+
+
+}
+?>
