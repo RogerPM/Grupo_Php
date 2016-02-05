ad9b46aa7f4e9dda65b06ec0e57e632f8525ae25
Edwin Estacio <p210estacioe@gmail.com>
2016-02-05 15:44:36 -0500

    Validado y Probado consulta general

--- AlimFaster/pages/DataBase.php
+++ AlimFaster/pages/DataBase.php
@@ -7,10 +7,10 @@ class dataBase
   public $isConnected;
   protected $datab;
 
-  private $host     = DB_HOST;// = localhost;
-  private $username = DB_USER;// = postgres;
-  private $password = DB_PASS;// = postgres;
-  private $dbname   = DB_NAME;// = Alimfaster;
+  private $host      = DB_HOST;
+  private $username  = DB_USER;
+  private $password  = DB_PASS;
+  private $dbname    = DB_NAME;
 
   public function __construct($username, $password, $host, $dbname, $options=array()){
     $this->isConnected = true;
--- AlimFaster/pages/ProductoDatos.php
+++ AlimFaster/pages/ProductoDatos.php
@@ -1,17 +1,17 @@
 <?php 
 include_once('Producto.php');
-include_once('Conexion.php');
+include_once('Collector.php');
 
-class ProductoDatos extends Conexion 
+class ProductoDatos extends Collector 
 {
 	function consultaGeneral(){
 		$rows = self::$db->getRows("SELECT * FROM producto");
-		$arrayProducto = array();
+		$arrayDemo = array();
 		foreach ($rows as $c){
 			$aux = new Producto($c{'codigo_producto'},$c{'descripcion'},$c{'costo'},$c{'pvp'},$c{'utilidad'},$c{'contenido_neto'},$c{'peso_neto'});
-			array_push($arrayProducto, $aux);
+			array_push($arrayDemo, $aux);
 		}
-	return $arrayProducto;
+	return $arrayDemo;
 	}
 
 }
