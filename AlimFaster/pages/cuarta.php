a90b7591932cdafb777caa59831c483d4a31341a
Edwin Estacio <lzcontre@espol.edu.ec>
2016-02-15 00:21:06 -0500

    Edwin Estacio

--- AlimFaster/pages/cuarta.php
+++ AlimFaster/pages/cuarta.php
@@ -1,106 +1,152 @@
-<?php require_once('php/conexion.php');?>
+<!--?php require_once('php/conexion.php');?-->
+<?php
+	session_start();
+?>
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyABcvzwpua9QIgDkzdG_afvS0IHC40g_Gs"></script>
-<script src="js/jquery-1.10.2.min.js"></script>
-<script src="js/initmap.js"></script>
-<title>Consulta y Reservacion en restaurante</title>
-<link href="css/lis.css" rel="stylesheet" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Refresh" content="300"> <!--Se refrescara la pagina cada 300 segundos es decir 5 minutos-->
+<meta name="application-name" content="Reserva de mesas,pedidos de menus personalizados  y pagos via online para restaurantes gourmet">
+<meta name="author" content="Liss Contreras, Lzcontre@espol.edu.ec" /> 
+<meta name="copyright" content="AlimFaster"> <!--Nombre de la compañia-->
+<meta name="organization" content="AlimFaster S.A." /> <!--Nombre de la Organizacion-->
+<meta name="language" content="es-ES" /> <!--Lenguaje de la pagina web español-->
+<meta name="classification" content="Reservas">
+<meta name="description" content="Reserva de espacio en un restaurante de gourmet asociado" /> <!--Descripcion de la pagina web-->
+<meta name="generator" content="Bloc de notas,gedit"> <!--Cual es la herramienta que se utiliza para editar, crear la pagina web-->
+<meta name="keywords" content="reserva de restaurantes gourmet,  reserva de pedido y pago online de restaurantes gourmet, reserva de un espacio en restaurantes gourmet" /> <!--Palabras claves para que los navegadores puedan identificar y rankear nuestra pagina web-->
+<meta name="robots" content="index,follow"> <!--etiqueta que sirve para que los bucadores puedan recorrer tu pagina web-->
+<link href="../css/styleRestaurant.css" rel="stylesheet" type="text/css">
+<link href="../css/bootstrap.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script>
+$(window).load(function(){
+$("#mapita").click(function(){
+	$(this).next().toggle();
+});
+});
+</script>
+<title>Formulario de Reserva Online - Restaurant</title>
 </head>
+<body >
+<!--Esto es para estilos responsive manueales -->
+<!--<div class="prueba">Probando</div>
+<div class="prueba">Probando</div>
+<div class="prueba">Probando</div>
+<div class="prueba alfa omega">Probando</div>
+-->
+<!-- boostrap -->
+<div class="jumbotron">
+<div class="bannerSuperior">
 
-<body onLoad="firstInitialize()">
-<div id='cargando' style='display:none; position:absolute; top:0; bottom:0; left:0; right:0; background-color:black; opacity:0.8;'>
-Cargando...</div>
-<h1>RESTAURANT</h1>
-<form id="formulario" class="col">
-	<h2>Datos de Reservación</h2>
-	<label>Donde le gustaría comer??</label>
-	 <select id="establecimiento"  name="establecimiento" required>
-        <option value="">Seleccione una opcion</option>
-        <?php
-		$query = "SELECT * FROM restaurante";
-		$res = pg_query($conexion, $query) or die("Error en la Consulta SQL");
+	<!-- Logo Y Texto Banner  -->
+<div class="jumbotron">
+<div class="banImgText">
+				 <div class="col-md-8">
+				<img src="../img/logotipo.png" title="Restaurante1"  height="100" width="240" alt="px">
+				</div>
+				<div class="col-md-4">
+				<h1>Restaurant</h1>
+				</div>
+</div>
+				</div>
+				
+<!-- Botones  -->
+<div class="jumbotron">
+<div class="botones">
 
-		//$res =mysql_query('SELECT * FROM establecimiento '); 
-		//while($filares =mysql_fetch_array($res)){
-		while($fila=pg_fetch_array($res)){
-			$esta= $fila['razon_social'];
-			/*$max= $filares['max_personas'];
-			$in= $filares['inicio_atencion'];
-			$fin= $filares['fin_atencion'];*/
-			echo "<option>".$esta."</option>";
-		}
-		?>
-    </select>
-    
-    <div>
-    	<label>Seleccione Sucursal/Restaurante</label>
-        <select id="cord" name="sucursal" required>
-        	<option value="">Seleccione una opcion</option>
-        </select>
-    </div>
+				<a href="../index.html"><button type="button" class="botonHomeNextBack"><img src="../img/home.png" height="40" width="40" alt="px"></button></a>
+				
+				<a href="tercera.html"><button type="button" class="botonHomeNextBack"><img src="../img/back.png" height="40" width="40" alt="px"></button></a>
+</div>
+</div>
 
-    <div id='horarioAtencion'>
-        <label>Horario de atencion</label>
-        <span></span>
-    </div>
-
-    <div>
-        <label>Fecha de Orden</label>
-        <input type="date" id="fecha" name="fechaOrden" required/>
-    </div>
-
-    <div>
-        <label>Hora de Orden</label>
-        <select id='horarioReserv' name="horaOrden" required>
-        	<option value="">Seleccione una opcion</option>
-        </select>
-    </div>
-
-    <div>
-        <label>Numero de Personas</label>
-        <select id="mesaPara" required name="mesaPara">
-            <option value="">Seleccione una opcion</option>
-            
-        </select>
-    </div>
-
-    <div>
-        <label>Mesas Disponibles para cantidad de personas a reservar</label>
-        <div class="mesas">
-            <span class="mesa3"> Mesa 3</span>
-            <span class="mesa4"> Mesa 4</span>
-            <span class="mesa5"> Mesa 5</span>
-            <span class="mesa1"> Mesa 1</span>
-            <span class="mesa2"> Mesa 2</span>
-            <span class="mesa5"> Mesa 5</span>
-            <span class="mesa3"> Mesa 3</span>
-            <span class="mesa2"> Mesa 2</span>
-            <span class="mesa4"> Mesa 4</span>
-            <span class="mesa1"> Mesa 1</span>
-            <div class="msg"></div>
-        </div>
+</div>
+</div>
+<div class="fondoPantalla" style ="width = 100%">
+<div class="container">
+    <div class="row">
         
+        <div class="col-md-8">
+        <div class="table-responsive">    
+			<form >
+				<h2>DATOS DE RESERVACION</h2>
+				<div class="divForm">
+				<label class="col-xs-3 " for="fecha">Fecha</label>
+				<input id="fecha" type="date" name="bday" />
+				</div>
+				
+				<div class="divForm">
+				<label class="col-xs-3" for="hora">Hora</label>
+				<input id="hora" type="time" name="usr_time" />
+				</div>
+			  
+				<div class="divForm">
+				<label class=" col-xs-3" for="npersona">No. de personas</label>
+				<input id="npersona" type="text" />
+				</div>
+			  
+				<div class="divForm">
+				<label class=" col-xs-3" for="agencia">Agencia</label>
+				<input id="agencia" type="text" />
+				<a href="#" id='mapita' ><img src="../img/pin.png" width='25' alt="px"></a>
+				<div style='display:none;'>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d15948.284331801431!2d-79.90246695!3d-2.1263467499999997!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sec!4v1451873305946" width="600" height="450" style="border:0" allowfullscreen></iframe>
+				</div>
+				</div>
+			 
+				<h2>DATOS DEL USUARIO</h2>
+				  <div class="divForm">
+				  <label class=" col-xs-3" for="nombre">Nombre</label>
+				  <input id="nombre" type="text" />
+				  </div>
+				  
+				  <div class="divForm">
+				  <label class=" col-xs-3" for="apellido">Apellido</label>
+				  <input id="apellido" type="text" />
+				  </div>
+
+				<div class="divForm">
+				<label class=" col-xs-3" for="cedula">Cedula o Ruc</label>
+				<input id="cedula" type="number" />
+				</div>
+					
+				<div class="divForm">	
+				<label class=" col-xs-3" for="telefono">Telefono</label>
+				<input id="telefono" maxlength="10" type="tel" />
+				</div>
+
+				<div class="divForm">				
+				<label class=" col-xs-3" for="nombre">Mail</label>
+				<input id="mail" type="text" />
+				</div>
+				
+				
+				<div class="divForm">
+				<label class=" col-xs-3" for="nombre">Cuenta PayPal</label>
+				<input id="cuenta" type="text" />
+				<a href="#" >Obteniendo</a>
+				</div>
+				
+				<div class="divFormBoton">
+					<a href="../index.html"><input class="objetoFormReservarForm" type="button" value="RESERVAR"/></a>
+				</div>
+			</form>	
+		</div>
+        </div>
     </div>
+</div>
 
-    <h2>DATOS DE USUARIO</h2>
-    <!-- Horario de atencion seria solo informacion segun sea el restaurante que se liste-->
-    <label>Nombre</label><input type="text" placeholder="Nombre" required id="nombre" name="nombre"/><br/>
-	<!-- Horario de atencion seria solo informacion segun sea el restaurante que se liste-->
-    <label>Apellidos</label><input type="text" placeholder="Apellido" required id="apellido" name="apellido"/><br/>
-    <label>Cedula</label><input type="text" placeholder="Cedula" required id="cedula" name="cedula"/><br/>
-    <label>Telefono</label><input type="text" placeholder="Telefono" required id="telefono" name="telefono"/><br/>
-    <label>Mail</label><input type="text" placeholder="Mail" required id="mail" name="mail"/><br/>
-    <label>Forma de Pago</label><input type="text" placeholder="Forma de Pago" required id="forma_pago" name="forma_pago"/><br/>
-    
-    <label></label><input type="submit" value="OK"/>
-</form>
 
-<div id="mimapa" class="col"></div>
+ 		
+		
+		
+</div>
 
-<div id="mostrario"></div>
+<div class="footer">
+Contáctenos: desarrollo@gmail.com.<br/> Ecuador 2015
+</div>
 </body>
-<script src="js/actions.js"></script>
+</html>
 </html>
