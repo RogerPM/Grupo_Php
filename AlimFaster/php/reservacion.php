a12c544b890eccf29a5493f015810ca729748603
liscontreras <lzcontre@espol.edu.ec>
2016-02-18 23:32:07 -0500

    Update reservacion.php

--- AlimFaster/php/reservacion.php
+++ AlimFaster/php/reservacion.php
@@ -63,6 +63,64 @@ if($bandera == "2"){
 			echo "Puedes reservar <br/>";
 		}
 	}
+	
+if($bandera == "3"){
+	$ced = $_POST['ced'];
+	$reserva ="SELECT * FROM reservacion WHERE cedula= '".$ced."'";
+	$reservaQuery =pg_query($conexion,$reserva);
+	
+	echo "<div class='tablita'>";
+	echo "<div><span>Cod.</span><span>Establecimiento</span> <span>Sucursal</span><span>Fecha Orden</span>
+			<span>Hora Orden</span><span>No. Personas</span><span>Nombre</span></div>";
+
+while($filareserva = pg_fetch_array($reservaQuery)){
+			 
+			$reserva_id	= $filareserva['reserva_id'];
+			$establecimiento= $filareserva['establecimiento'];	
+			$sucursal= $filareserva['sucursal'];	
+			$fecha_orden=$filareserva['fecha_orden'];	
+			$hora_orden	=$filareserva['hora_orden'];	
+			$numero_personas=$filareserva['numero_personas'];	
+			$nombre=$filareserva['nombre'];
+			//echo "<div><span>cod.</span><span>descripcion</span> <span>costo</span></div>";
+			
+			echo "<div>";
+			echo "<span><input type='text' name='id' value='".$reserva_id."'/></span>";
+			echo "<span><input type='text' name='' value='".$establecimiento."'/></span>";
+			echo "<span><input type='text' name='' value='".$sucursal."'/></span>";
+			echo "<span><input type='text' name='' value='".$fecha_orden."'/></span>";
+			echo "<span><input type='text' name='' value='".$hora_orden."'/></span>";
+			echo "<span><input type='text' id='nop' value='".$numero_personas."'/></span>";
+			echo "<span><input type='text' id='nom' value='".$nombre."'/></span>";
+			echo "<span><input type='button' value='Borrar' onClick='borra($(this))'> <input type='button' value='Modificar' onClick='modifica($(this))'></span>";
+			echo "</div>";		
+						
+	}			
+	echo "</div>";	
+}	
+
+if($bandera == "4"){
+	$numerito = $_POST['numerito'];
+	$delete ="DELETE FROM reservacion WHERE reserva_id= '".$numerito."'";
+	$deleteQuery =pg_query($conexion,$delete);
+	if($deleteQuery ){
+	echo "<script>alert('registro eliminado')</script>";
+	}	
+}	
+
+if($bandera == "5"){
+	$numerito = $_POST['numerito'];
+	$nopersonas = $_POST['nopersonas'];
+	$nombre = $_POST['nombre'];
+	$delete ="UPDATE reservacion SET numero_personas='".$nopersonas."', nombre='".$nombre."' WHERE reserva_id= '".$numerito."'";
+	$deleteQuery =pg_query($conexion,$delete);
+	if($deleteQuery){
+	echo "<script>alert('registro actualizado')</script>";
+	}	
+}	
+
+
+	
 }
 ?>
 
